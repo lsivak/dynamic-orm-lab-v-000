ab878d47257a47bdeade99a170ec33fb0a060b45
Lara Sanders <larasanders@gmail.com>
2017-12-02 21:52:53 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -15,15 +15,17 @@ class InteractiveRecord
     column_names = []
     table_info.each do |column|
       column_names<<column["name"]
-      attr_accessor column_name.to_s
     end
     column_names.compact
   end
 
-
+def table_name_for_insert
+  self.class.table_name
+end
   def initialize(options = {})
   options.each do |property, value|
     self.send("#{property}=, value")
+      attr_accessor column_name.to_s
   end
 end
 end
