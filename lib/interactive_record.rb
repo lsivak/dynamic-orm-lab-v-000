bbb8bf2bb163adc3e150d6b1a1c7ff6753accd7c
Lara Sanders <larasanders@gmail.com>
2017-12-02 21:55:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,6 +19,11 @@ class InteractiveRecord
     column_names.compact
   end
 
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+end
+
 def table_name_for_insert
   self.class.table_name
 end
