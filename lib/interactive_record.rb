cefa98ed296f52974f612a84ef66ce81f57747ad
Lara Sanders <larasanders@gmail.com>
2017-12-02 21:52:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,6 +22,7 @@ class InteractiveRecord
 def table_name_for_insert
   self.class.table_name
 end
+
   def initialize(options = {})
   options.each do |property, value|
     self.send("#{property}=, value")
